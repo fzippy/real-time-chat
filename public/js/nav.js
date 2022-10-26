0e6f84a7260b4f50360a10d1b92ceec1027b4a76
fzippy <hk2011khan@gmail.com>
2022-10-26 23:42:21 +0100

    bug fix

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,11 +1,13 @@
 const burger = document.querySelector("#burger");
 const burger2 = document.querySelector("#burger2");
 const nav = document.querySelector("nav");
+const navChildren = document.querySelector("#navChildren");
 
 burger.addEventListener("click", () => {
-  console.log("y");
+  navChildren.style.display = "block";
   nav.style.width = "225px";
 });
 burger2.addEventListener("click", () => {
   nav.style.width = "0px";
+  navChildren.style.display = "none";
 });
